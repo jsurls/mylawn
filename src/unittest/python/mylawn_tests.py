69490a414dcd720583bdddc1b8ae17c9dd996eea
jsurls <jsurls@gmail.com>
2019-05-18 22:13:18 -0500

    Add unit tests for mylawn

--- src/unittest/python/mylawn_tests.py
+++ src/unittest/python/mylawn_tests.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
-import unittest
 
+import unittest
+import mock
+
+import mylawn
 from mylawn import water_guidance, mow_guidance
 
 
@@ -16,3 +19,25 @@ class MyLawnTests(unittest.TestCase):
         self.assertEqual(mow_guidance(95), "regular")
         self.assertEqual(mow_guidance(84), "regular")
         self.assertEqual(mow_guidance(83), "lowest")
+
+    def test_mow_message(self):
+        message = mylawn.mow_message(100)
+
+        self.assertIsNotNone(message)
+        self.assertTrue(message.startswith("You should mow at the"))
+
+    def test_weather_message(self):
+        message = mylawn.weather_message(100, 5)
+
+        self.assertIsNotNone(message)
+        self.assertTrue(message.startswith("The avg high was"))
+
+    @mock.patch('mylawn.get_weather_data')
+    def test_get_water_info(self, mock_get_weather_data):
+        mock_get_weather_data.return_value = (100, 5)
+
+        message = mylawn.get_water_info('station_id')
+
+        self.assertIsNotNone(message)
+        self.assertEquals(len(message), 3)
+        self.assertTrue(message[0].startswith("The avg high was"))
